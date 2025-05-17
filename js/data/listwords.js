eb8ff113955d63e4090d7640e973e280a6aeeb70
JuanfeP2004 <juanf.ramirezc@upb.edu.co>
2025-05-16 19:52:39 -0500

    Se agrego la lista de palabras e imagenes a usar

---
+++ js/data/listwords.js
@@ -0,0 +1,46 @@
+class ListWords {
+
+    easy_words = [
+        { word: 'luna', image: 'img/easy/luna.jpg'},
+        { word: 'rosa', image: 'img/easy/rosa.jpg'},
+        { word: 'casa', image: 'img/easy/casa.jpg'},
+        { word: 'auto', image: 'img/easy/auto.jpg'},
+        { word: 'playa', image: 'img/easy/playa.jpg'},
+        { word: 'libro', image: 'img/easy/libro.jpg'},
+        { word: 'arbol', image: 'img/easy/arbol.jpg'},
+        { word: 'avion', image: 'img/easy/avion.jpg'},
+        { word: 'silla', image: 'img/easy/silla.jpg'},
+        { word: 'fresa', image: 'img/easy/fresa.jpg'},
+        { word: 'caja', image: 'img/easy/caja.jpg'},
+    ]
+
+    medium_words = [
+        { word: 'paraguas', image: 'img/medium/paraguas.jpg' },
+        { word: 'aguacate', image: 'img/medium/aguacate.jpg' },
+        { word: 'tormenta', image: 'img/medium/tormenta.jpg' },
+        { word: 'planeta', image: 'img/medium/planeta.jpg' },
+        { word: 'muelle', image: 'img/medium/muelle.jpg' },
+        { word: 'esfera', image: 'img/medium/esfera.jpg' },
+        { word: 'oceano', image: 'img/medium/oceano.jpg' },
+        { word: 'tenedor', image: 'img/medium/tenedor.jpg' },
+        { word: 'sombrero', image: 'img/medium/sombrero.jpg' },
+        { word: 'espejo', image: 'img/medium/espejo.jpg' },
+        { word: 'montaña', image: 'img/medium/montaña.jpg'}
+    ]
+
+    hard_words = [
+        { word: 'explosion', image: 'img/hard/explosion.jpg'},
+        { word: 'amanecer', image: 'img/hard/amanecer.jpg'},
+        { word: 'laboratorio', image: 'img/hard/laboratorio.jpg'},
+        { word: 'camuflaje', image: 'img/hard/camuflaje.jpg'},
+        { word: 'invernadero', image: 'img/hard/invernadero.jpg'},
+        { word: 'biblioteca', image: 'img/hard/biblioteca.jpg'},
+        { word: 'marioneta', image: 'img/hard/marioneta.jpg'},
+        { word: 'fotografia', image: 'img/hard/fotografia.jpg'},
+        { word: 'escritorio', image: 'img/hard/escritorio.jpg'},
+        { word: 'aeropuerto', image: 'img/hard/aeropuerto.jpg'},
+        { word: 'esqueleto', image: 'img/hard/esqueleto.jpg'},
+    ]
+}
+
+export default ListWords
